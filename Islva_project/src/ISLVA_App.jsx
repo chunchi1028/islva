0744890719cbc8fbe2f4cb12817c5578744436bc
chunchi1028 <wsjennt1028@gmail.com>
2025-09-01 14:24:36 +0800

    0901

--- Islva_project/src/ISLVA_App.jsx
+++ Islva_project/src/ISLVA_App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./component/NavBar.jsx"
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import Glossary from "./pages/Glossary.jsx" 
+import Shop from "./pages/Shop.jsx";
+import Member from "./pages/Member.jsx";
 
 const ISLVA_App = () => {
   return (
-    <div>ISLVA_App</div>
+    <div className="wrap">
+    <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </div>
   )
 }
 
