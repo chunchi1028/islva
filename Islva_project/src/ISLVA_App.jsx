104b68d7adbb0befae1904b1f3452d96eda5b524
chunchi1028 <wsjennt1028@gmail.com>
2025-09-01 10:06:48 +0800

    first commit

---
+++ Islva_project/src/ISLVA_App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ISLVA_App = () => {
+  return (
+    <div>ISLVA_App</div>
+  )
+}
+
+export default ISLVA_App
---
+++ Islva_project/src/main.jsx
@@ -0,0 +1,14 @@
+import { HashRouter } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './ISLVA_App'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HashRouter>
+    <App />
+    </HashRouter>
+    
+  </StrictMode>,
+)
